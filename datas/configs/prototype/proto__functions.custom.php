4871d734fc1af5b0b4578c47ff9d98cfcbdd79ef
NikWebit <88228293+NikWebIt@users.noreply.github.com>
2021-11-17 01:12:08 +0200

    Новая директория с прототипами файлов

---
+++ datas/configs/prototype/proto__functions.custom.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Базовый файл функций.
+ *
+ * @package API - Functions
+ *
+ * @author NikWebit <https://cotont.ru/contact>
+ * @copyright (c) Cotont CMS <https://cotont.ru>
+ * @license https://github.com/NikWebit/Cotont/blob/master/LICENCE BSD-3-Clause
+ */
+if (defined('COT_CODE')) {
+    /**
+     * Ваши пользовательские функции, для подключения нужно:
+     *
+      - скопировать этот файл в /app/libraries/custom/
+      - можно сразу добавить в него свои функции, или позже
+      - переименовать в functions.custom.php (убрать proto__ префикс)
+      - открыть главный файл конфигурации /datas/configs/config.php
+      - включить опцию: $cfg['customfuncs'] = true, можно пользоваться.
+     */
+}
